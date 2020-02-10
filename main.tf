f63eb52ecb50f5680c191a9bd89b409ce04dd3c6
George Watkins <g@watkins.io>
2020-02-10 12:22:44 -0800

    adding TCP profile to HTTP virtual server

--- main.tf
+++ main.tf
@@ -7,11 +7,14 @@ provider "bigip" {
 # HTTP virtual server configuration
 
 resource "bigip_ltm_virtual_server" "terraform_test_vs_http" {
-  name                       = "/Common/terraform_test_vs_http"
-  destination                = "10.84.100.100"
-  description                = "Terraform Test HTTP Virtual Server"
-  port                       = 80
-  profiles                   = ["/Common/terraform_test_profile_http"]
+  name        = "/Common/terraform_test_vs_http"
+  destination = "10.84.100.100"
+  description = "Terraform Test HTTP Virtual Server"
+  port        = 80
+  profiles = [
+    "/Common/tcp",
+    "/Common/terraform_test_profile_http"
+  ]
   pool                       = "/Common/terraform_test_pool_http"
   source_address_translation = "automap"
   depends_on = [
@@ -24,42 +27,56 @@ resource "bigip_ltm_pool" "terraform_test_pool_http" {
   name                = "/Common/terraform_test_pool_http"
   load_balancing_mode = "round-robin"
   description         = "Terraform Test HTTP Pool"
-  monitors            = ["/Common/http"]
-  allow_snat          = "yes"
-  allow_nat           = "yes"
+  monitors = [
+    "/Common/http"
+  ]
+  allow_snat = "yes"
+  allow_nat  = "yes"
 }
 
 # HTTPS virtual server configuration
 
 resource "bigip_ltm_virtual_server" "https" {
-  name                       = "/Common/terraform_test_vs_https"
-  destination                = "10.84.100.101"
-  description                = "Terraform Test HTTPS Virtual Server"
-  port                       = 443
-  client_profiles            = ["/Common/clientssl"]
-  server_profiles            = ["/Common/serverssl"]
+  name        = "/Common/terraform_test_vs_https"
+  destination = "10.84.100.101"
+  description = "Terraform Test HTTPS Virtual Server"
+  port        = 443
+  client_profiles = [
+    "/Common/clientssl"
+  ]
+  server_profiles = [
+    "/Common/serverssl"
+  ]
   pool                       = "/Common/terraform_test_pool_https"
   source_address_translation = "automap"
-  depends_on                 = [bigip_ltm_pool.terraform_test_pool_https]
+  depends_on = [
+    bigip_ltm_pool.terraform_test_pool_https
+  ]
 }
 
 resource "bigip_ltm_pool" "terraform_test_pool_https" {
   name                = "/Common/terraform_test_pool_https"
   load_balancing_mode = "round-robin"
   description         = "Terraform Test HTTPS Pool"
-  monitors            = ["/Common/https"]
-  allow_snat          = "yes"
-  allow_nat           = "yes"
+  monitors = [
+    "/Common/https"
+  ]
+  allow_snat = "yes"
+  allow_nat  = "yes"
 }
 
 # HTTP profile configuration
 
 resource "bigip_ltm_profile_http" "terraform_test_profile_http" {
-  name                  = "/Common/terraform_test_profile_http"
-  defaults_from         = "/Common/http"
-  description           = "Terraform Test HTTP Profile"
-  fallback_host         = "https://f5.com/"
-  fallback_status_codes = ["400", "500", "300"]
+  name          = "/Common/terraform_test_profile_http"
+  defaults_from = "/Common/http"
+  description   = "Terraform Test HTTP Profile"
+  fallback_host = "https://f5.com/"
+  fallback_status_codes = [
+    "400",
+    "500",
+    "300"
+  ]
 }
 
 # SSL profile configuration
@@ -67,15 +84,15 @@ resource "bigip_ltm_profile_http" "terraform_test_profile_http" {
 # SSL certificate configuration
 
 resource "bigip_ssl_key" "terraform_test_ssl_key" {
-        name = "example.com"
-        content = tls_private_key.terraform_test_tls_private_key.private_key_pem
-        partition = "Common"
+  name      = "example.com"
+  content   = tls_private_key.terraform_test_tls_private_key.private_key_pem
+  partition = "Common"
 }
 
 resource "bigip_ssl_certificate" "terraform_test_ssl_certificate" {
-        name = "example.com"
-        content = tls_self_signed_cert.terraform_test_tls_self_signed_cert.cert_pem
-        partition = "Common"
+  name      = "example.com"
+  content   = tls_self_signed_cert.terraform_test_tls_self_signed_cert.cert_pem
+  partition = "Common"
 }
 
 # SSL certificate generator
