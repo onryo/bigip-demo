18ce81c3e0e42ac7c1c16dc471c5d31a7d2b1d0b
George Watkins <g@watkins.io>
2020-02-10 17:12:45 -0800

    Removed explicit dependencies from HTTP example

--- bigip_test_vs_http.tf
+++ bigip_test_vs_http.tf
@@ -8,14 +8,10 @@ resource "bigip_ltm_virtual_server" "terraform_test_vs_http" {
   port        = 80
   profiles = [
     "/Common/tcp",
-    "/Common/terraform_test_profile_http"
+    bigip_ltm_profile_http.terraform_test_profile_http.name
   ]
-  pool                       = "/Common/terraform_test_pool_http"
+  pool                       = bigip_ltm_pool.terraform_test_pool_http.name
   source_address_translation = "automap"
-  depends_on = [
-    bigip_ltm_pool.terraform_test_pool_http,
-    bigip_ltm_profile_http.terraform_test_profile_http
-  ]
 }
 
 # HTTP pool configuration
@@ -25,14 +21,10 @@ resource "bigip_ltm_pool" "terraform_test_pool_http" {
   load_balancing_mode = "round-robin"
   description         = "Terraform Test HTTP Pool"
   monitors = [
-    "/Common/terraform_test_http_monitor"
+    bigip_ltm_monitor.terraform_test_http_monitor.name
   ]
   allow_snat = "yes"
   allow_nat  = "yes"
-
-  depends_on = [
-    bigip_ltm_monitor.terraform_test_http_monitor
-  ]
 }
 
 # HTTP node/pool attachment
@@ -40,9 +32,6 @@ resource "bigip_ltm_pool" "terraform_test_pool_http" {
 resource "bigip_ltm_pool_attachment" "terraform_test_node_attach_http" {
   pool = bigip_ltm_pool.terraform_test_pool_http.name
   node = "${bigip_ltm_node.terraform_test_node.name}:80"
-  depends_on = [
-    bigip_ltm_node.terraform_test_node
-  ]
 }
 
 # HTTP monitor configuration
